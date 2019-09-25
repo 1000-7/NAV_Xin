917e3aae200eeb8299a8020ec076d59dc154565d
xavior.wx <xin1996whu@icloud.com>
2019-09-25 13:26:40 +0800

    mysql文件整理出来

--- code/mysql/mysql_data_process.py
+++ code/mysql/mysql_data_process.py
@@ -1,9 +1,9 @@
 import pymysql
-from others.logging import logger
+from others.logging import logger, init_logger
 
 # db = pymysql.connect("localhost", "root", "irlab2017", "graduate", charset='utf8')
 db = pymysql.connect("localhost", "root", "wx1996", "graduate", charset='utf8')
-
+init_logger("../../logs/test.log")
 # 使用cursor()方法获取操作游标
 cursor = db.cursor()
 
@@ -18,4 +18,11 @@ for iter in range(0, 10):
     # 获取所有记录列表
     results = cursor.fetchall()
     for row in results:
-        type1 = row[6]
+        data_id = row[0]
+        paper_section = row[2].replace("- ", "")
+        summary_sentence = row[3].replace("- ", "")
+        with open('/Users/unclewang/PycharmProjects/NAV_Xin/data/' + str(data_id) + '.story', 'w') as fw:
+            fw.write(paper_section + "\n")
+            fw.write("@highlight")
+            fw.write(summary_sentence)
+        break
