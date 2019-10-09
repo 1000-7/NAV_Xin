3f524b88ec54793d8d062a0078226ebd949942d2
xavior.wx <xin1996whu@icloud.com>
2019-10-09 16:20:57 +0800

    mysql数据重新生成

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # NAV_Xin
+
+
+
+python train.py -mode validate -bert_data_path /home/ljw/wangxin/bert_file/bert_data/ss -model_path ../models/bert_transformer  -visible_gpus 0  -gpu_ranks 0 -batch_size 30000  -log_file ../logs/validate.log  -result_path ../results/cnndm -test_all -block_trigram true
+
+python train.py -mode test -bert_data_path /home/ljw/wangxin/bert_file/bert_data/ss -model_path ../models/bert_transformer  -visible_gpus 0  -gpu_ranks 0 -batch_size 30000  -log_file ../logs/test.log  -result_path ../results/test -test_from ../models/bert_transformer/model_step_50000.pt -test_all -block_trigram true
--- code/mysql/mysql_data_process.py
+++ code/mysql/mysql_data_process.py
@@ -1,5 +1,34 @@
 import pymysql
 from others.logging import logger, init_logger
+import re
+
+
+def clear(s):
+    s = s.replace("%", "").replace(">", " ") \
+        .replace("[]", "").replace(" ± ", "").replace(", . ", " ").replace(
+        "–)", ")").replace(".,", ",").replace("?-", "") \
+        .replace("-?", "").replace("/", "").replace(". ·", "").replace("=", "").replace(
+        "?.", "").replace(", ,", "").replace("(.)", "") \
+        .replace("//", "").replace("<", "").replace("  ", " ") \
+        .replace(" - ", "").replace(".-", " ").replace("...", "").replace("..", ".") \
+        .replace("+-", "").replace("-.", "").replace(" . ", ".").replace("°", "")
+
+    s = re.sub("\\([^()]*\\)", "", s)
+    s = re.sub("\\[[^()]*\\]", "", s)
+    return s
+
+
+def is_filter(s):
+    return s.find("............") >= 0
+
+
+def is_filter_paper_section(s):
+    return is_filter(s) or len(s.split(" ") < 50)
+
+
+def is_filter_summary(s):
+    return is_filter(s) or len(s.split(" ") < 10) or len(s.split(" ")) > 150
+
 
 db = pymysql.connect("localhost", "root", "irlab2017", "graduate", charset='utf8')
 # db = pymysql.connect("localhost", "root", "wx1996", "graduate", charset='utf8')
@@ -20,8 +49,14 @@ for iter in range(0, 11):
     finishNum = 0
     for row in results:
         data_id = row[0]
-        paper_section = row[2].replace("- ", "")
-        summary_sentence = row[3].replace("- ", "").replace("\n", "")
+        paper_section = clear(row[2].replace("- ", ""))
+        summary_sentence = clear(row[3].replace("- ", "").replace("\n", ""))
+        if len(paper_section.split(" ")) - len(summary_sentence.split(" ")) < 50:
+            continue
+
+        if is_filter_paper_section(paper_section) or is_filter_summary(summary_sentence):
+            continue
+
         with open('/home/wangxin/PycharmProjects/nav_story/' + str(data_id) + '.story', 'w') as fw:
             fw.write(paper_section + "\n")
             fw.write("@highlight" + "\n")
